4355e69768ef61b66c767773014c9e9dd75c62c0
roman-glushkov <roma.glushkov.vuz@mail.ru>
2025-11-02 17:23:03 +0300

    тулбар

--- LAB_5/src/common/components/Toolbar/parts/Group.tsx
+++ LAB_5/src/common/components/Toolbar/parts/Group.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GroupKey } from '../hooks/useState';
 import { GROUPS } from '../constants/config';
 import TemplatePopup from './TemplatePopup';
 import ColorSection from './ColorSection';
+import TextOptionsPopup from './TextOptionsPopup';
+import TextAlignPopup from './TextAlignPopup';
+import { TEXT_SIZE_OPTIONS, LINE_HEIGHT_OPTIONS } from '../constants/textOptions';
 
 interface Props {
   activeGroup: GroupKey;
@@ -16,6 +19,7 @@ interface Props {
   handleBackgroundColorClick: () => void;
   handleTemplateSelect: (template: string) => void;
   handleColorSelect: (type: 'text' | 'fill' | 'background', color: string) => void;
+  handleTextOptionSelect: (key: string) => void;
   onAction: (action: string) => void;
 }
 
@@ -31,23 +35,32 @@ export default function ToolbarGroup({
   handleBackgroundColorClick,
   handleTemplateSelect,
   handleColorSelect,
+  handleTextOptionSelect,
   onAction,
 }: Props) {
+  const [activeTextOption, setActiveTextOption] = useState<string | null>(null);
+
+  const handleButtonClick = (action: string) => {
+    if (action === 'Добавить слайд') handleAddSlideClick();
+    else if (action === 'Изменить цвет текста') handleTextColorClick();
+    else if (action === 'Изменить фон текста') handleFillColorClick();
+    else if (action === 'Изменить фон слайда') handleBackgroundColorClick();
+    else if (
+      action === 'Изменить размер текста' ||
+      action === 'text-align' ||
+      action === 'Изменить межстрочный интервал'
+    ) {
+      setActiveTextOption(activeTextOption === action ? null : action);
+    } else {
+      onAction(action);
+    }
+  };
+
   return (
     <div className="toolbar-group">
       {GROUPS[activeGroup].map(({ label, action }) => (
         <div key={action} className="toolbar-button-wrapper">
-          <button
-            onClick={() => {
-              if (action === 'Добавить слайд') handleAddSlideClick();
-              else if (action === 'Изменить цвет текста') handleTextColorClick();
-              else if (action === 'Изменить фон текста') handleFillColorClick();
-              else if (action === 'Изменить фон слайда') handleBackgroundColorClick();
-              else onAction(action);
-            }}
-          >
-            {label}
-          </button>
+          <button onClick={() => handleButtonClick(action)}>{label}</button>
 
           {action === 'Добавить слайд' && showTemplates && (
             <TemplatePopup onSelect={handleTemplateSelect} />
@@ -63,6 +76,35 @@ export default function ToolbarGroup({
 
           {action === 'Изменить фон слайда' && showBackgroundColorPicker && (
             <ColorSection type="background" onSelect={handleColorSelect} />
+          )}
+
+          {action === 'Изменить размер текста' && activeTextOption === action && (
+            <TextOptionsPopup
+              options={TEXT_SIZE_OPTIONS}
+              onSelect={(key) => {
+                handleTextOptionSelect(key);
+                setActiveTextOption(null);
+              }}
+            />
+          )}
+
+          {action === 'text-align' && activeTextOption === action && (
+            <TextAlignPopup
+              onSelect={(key) => {
+                handleTextOptionSelect(key);
+                setActiveTextOption(null);
+              }}
+            />
+          )}
+
+          {action === 'Изменить межстрочный интервал' && activeTextOption === action && (
+            <TextOptionsPopup
+              options={LINE_HEIGHT_OPTIONS}
+              onSelect={(key) => {
+                handleTextOptionSelect(key);
+                setActiveTextOption(null);
+              }}
+            />
           )}
         </div>
       ))}
